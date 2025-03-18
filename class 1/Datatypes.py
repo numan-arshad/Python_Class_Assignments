bd4202855951f8b25273aa60bf3b30c9d092e1f6
Numan Arshad <154283305+numan-arshad@users.noreply.github.com>
2025-03-19 04:49:39 +0500

    Add files via upload

---
+++ class 1/Datatypes.py
@@ -0,0 +1,59 @@
+print ("hello world")
+
+#/ Data Types 
+#/ String
+name = "Numan Arshad"
+print (name)
+
+#/ integers
+num1 = -25
+num2 = 25
+print (num1)
+print (num2)
+
+#/ Float (decimal numbers)
+float = 3.14
+print (float)
+
+#/ Boolean (True, False)
+is_true = True
+is_false = False
+print (is_true)
+print (is_false)
+
+#/ List (array)
+list_1 = [1,2,3]
+list_2 = ["Apple","Mango","Cherry"]
+print (list_1)
+print (list_2)
+
+#/ Tuple
+tuple = ("umar",True,4,5)
+print (tuple)
+
+#/ Dictionary (obj : key value pair)
+name = "anusha"
+city = "karachi"
+country = "pakistan"
+print (name)
+print (city)
+print (country)
+
+#/ Set (unique data)
+set = {1,2,3,4}
+set.add(5)
+print (set)
+
+#/ Frozen set 
+set = frozenset({1,2,3})
+dict = {set:"Numan Arshad"}
+print (dict)
+
+#/ None
+x = None
+if x is None:
+    print("x has no value")
+else:
+    print ("x has {x} value")
+
+
